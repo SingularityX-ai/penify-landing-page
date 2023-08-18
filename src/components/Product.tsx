a135e519e92cea1531e3067b672702fe005e76dc
sumansaurabh <ss.sumansaurabh92@gmail.com>
2023-08-18 21:47:33 +0530

    fix small bug

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -5,7 +5,7 @@ import Divider from './Divider';
 
 const Product = () => {
   const { product } = config;
-  const [firstItem, secondItem] = product.items;
+  const [firstItem] = product.items;
 
   return (
     <section className={`bg-background py-8`} id="product">
@@ -31,25 +31,6 @@ const Product = () => {
               src={firstItem?.img}
               alt={firstItem?.title}
             />
-          </div>
-        </div>
-        <div className={`flex flex-wrap flex-col-reverse sm:flex-row`}>
-          <div className={`w-full sm:w-1/2 p-6`}>
-            <img
-              className="h-6/6"
-              src={secondItem?.img}
-              alt={secondItem?.title}
-            />
-          </div>
-          <div className={`w-full sm:w-1/2 p-6 mt-20`}>
-            <div className={`align-middle`}>
-              <h3
-                className={`text-3xl text-gray-800 font-bold leading-none mb-3`}
-              >
-                {secondItem?.title}
-              </h3>
-              <p className={`text-gray-600 mb-8`}>{secondItem?.description}</p>
-            </div>
           </div>
         </div>
       </div>
