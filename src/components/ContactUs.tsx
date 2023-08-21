1a5fa50b5876fce6975c9aad3501b3e826f481de
sumansaurabh <ss.sumansaurabh92@gmail.com>
2023-08-21 20:39:59 +0530

    privacy policy added

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 
-import config from '../config/index.json';
+import config from '../config/config';
+import configJson from '../config/index.json';
 import ContactUsBox from './ContactUsBox';
 import Divider from './Divider';
 
 const ContactUs = () => {
-  const { contactus } = config;
+  const { contactus } = configJson;
   const [firstItem] = contactus.items;
+  // const apiUrl = config[environment]?.apiUrl;
 
+  const privacyPolicyUrl = `${config.production?.apiUrl}/privacy-policy`;
   return (
-    <section className={`bg-background py-8`} id="contactus">
+    <section
+      className={`bg-background py-8 justify-center flex flex-col items-center`}
+      id="contactus"
+    >
       <div className={`container max-w-5xl mx-auto m-8`}>
         <h1
           className={`w-full my-2 text-5xl font-bold leading-tight text-center text-primary`}
@@ -30,6 +36,14 @@ const ContactUs = () => {
             />
           </div>
         </div>
+      </div>
+      <div className="flex items-center mt-6 text-center">
+        <p className="mt-6 text-xs lg:text-sm leading-none text-gray-900 dark:text-gray-50">
+          &copy; {new Date().getFullYear()} Property of Snorkell.ai |{' '}
+          <a target="_blank" href={privacyPolicyUrl} rel="noreferrer">
+            Terms and Conditions
+          </a>
+        </p>
       </div>
     </section>
   );
