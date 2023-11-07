cf1042d805000e4fec89f965f34b35b787dc266b
Mayur Dayal <mayur.dayal97@gmail.com>
2023-11-07 13:23:54 +0530

    init repo

---
+++ src/pages/_app.tsx
@@ -0,0 +1,27 @@
+// bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// font awesome 6
+import "public/font-awesome/css/all.min.css";
+
+// aos
+import "aos/dist/aos.css";
+
+// styles
+import "@/styles/main.scss";
+
+import type { AppProps } from "next/app";
+import { Suspense, useEffect } from "react";
+import Aos from "aos";
+
+export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    Aos.init();
+  }, []);
+
+  return (
+    <Suspense>
+      <Component {...pageProps} />
+    </Suspense>
+  );
+}
