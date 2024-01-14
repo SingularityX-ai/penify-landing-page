797ab38c367568f11cdf88cc7d53b97626153e5a
sumansaurabh <ss.sumansaurabh92@gmail.com>
2024-01-15 02:25:06 +0530

    fixed

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   env: {
     GA_MEASUREMENT_ID: "G-NQRNJW5NS7",
   },
   images: {
-    unoptimized: true,
+    unoptimized: false,
   },
   reactStrictMode: true,
-  output: "export",
 };
 
 module.exports = nextConfig;
