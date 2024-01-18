694df39b5cb224fe00b07591404d807978c19558
sumansaurabh <ss.sumansaurabh92@gmail.com>
2024-01-19 03:41:43 +0530

    fix the indent

--- src/components/container/tour/Tour.tsx
+++ src/components/container/tour/Tour.tsx
@@ -2,7 +2,7 @@ import { trackVideoStart } from "@/utils/gtag";
 import React, { forwardRef, useEffect, useRef, useState } from "react";
 
 // used function expression for working of scrolling effect
-const Tour = forwardRef<HTMLDivElement>(function (_,ref) {
+const Tour = forwardRef<HTMLDivElement>(function (_, ref) {
   const videoRef = useRef<HTMLDivElement>(null);
   const [autoplay, setAutoplay] = useState(false);
 
@@ -64,9 +64,45 @@ const Tour = forwardRef<HTMLDivElement>(function (_,ref) {
             <div className="tour__frame">
               <div className="frame__top">
                 <div className="mac__btns">
-                  <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="6" cy="6" r="6" fill="#FF605C"/></svg> 
-                  <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="6" cy="6" r="6" fill="#FFBD44" fillOpacity="0.866667"/></svg> 
-                  <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="6" cy="6" r="6" fill="#00CA4E" fillOpacity="0.866667"/></svg>
+                  <svg
+                    width="12"
+                    height="12"
+                    viewBox="0 0 12 12"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <circle cx="6" cy="6" r="6" fill="#FF605C" />
+                  </svg>
+                  <svg
+                    width="12"
+                    height="12"
+                    viewBox="0 0 12 12"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <circle
+                      cx="6"
+                      cy="6"
+                      r="6"
+                      fill="#FFBD44"
+                      fillOpacity="0.866667"
+                    />
+                  </svg>
+                  <svg
+                    width="12"
+                    height="12"
+                    viewBox="0 0 12 12"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <circle
+                      cx="6"
+                      cy="6"
+                      r="6"
+                      fill="#00CA4E"
+                      fillOpacity="0.866667"
+                    />
+                  </svg>
                 </div>
 
                 <div className="mac__title">
@@ -74,12 +110,33 @@ const Tour = forwardRef<HTMLDivElement>(function (_,ref) {
                 </div>
 
                 <div className="mac__code">
-                  <svg width="28" height="18" viewBox="0 0 28 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.99967 2.33331L2.33301 8.48715L8.99967 15.6666M18.9997 2.33331L25.6663 8.48715L18.9997 15.6666" stroke="#445268" strokeWidth="3.33333" strokeLinecap="round" strokeLinejoin="round"/></svg> 
+                  <svg
+                    width="28"
+                    height="18"
+                    viewBox="0 0 28 18"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M8.99967 2.33331L2.33301 8.48715L8.99967 15.6666M18.9997 2.33331L25.6663 8.48715L18.9997 15.6666"
+                      stroke="#445268"
+                      strokeWidth="3.33333"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
                 </div>
               </div>
 
               <div className="frame__mid" ref={videoRef}>
-                <iframe src={`https://www.youtube.com/embed/s32GS0glydA?autoplay=${Number(autoplay)}&loop=1&rel=0&fs=0&playlist=s32GS0glydA`} height="480" title="Snorkell Trailer"></iframe>
+                <iframe
+                  loading="lazy"
+                  src={`https://www.youtube.com/embed/s32GS0glydA?autoplay=${Number(
+                    autoplay
+                  )}&loop=1&rel=0&fs=0&playlist=s32GS0glydA`}
+                  height="480"
+                  title="Snorkell Trailer"
+                ></iframe>
               </div>
             </div>
           </div>
