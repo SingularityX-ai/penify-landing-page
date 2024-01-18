8f01e4e00bcdc67700a5d41afbee9bb829009288
sumansaurabh <ss.sumansaurabh92@gmail.com>
2024-01-18 13:31:13 +0530

    fix lables

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 // import ScrollProgress from "./ScrollProgress";
 import Header from "./Header";
 import Footer from "./Footer";
-import Link from "next/link";
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -25,8 +24,8 @@ export default function Layout({ children }: LayoutProps) {
           content="Snorkell.ai, ai docstring, Automated Documentation, GitHub Integration, Source Code Documentation, Intelligent Tracking, Smart Generation, Programming Languages, Python, JavaScript, TypeScript, Java, Kotlin, Real-Time Documentation, Privacy-Focused"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Link rel="icon" href="/favicon.ico" aria-label="Icon" />
-        <Link rel="canonical" href="https://www.snorkell.ai/"  aria-label="Snorkell Url"/>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="canonical" href="https://www.snorkell.ai/" />
       </Head>
 
         <div className={`my-app home-dark ${isNavOpen ? "body-active" : ""}`}>          
