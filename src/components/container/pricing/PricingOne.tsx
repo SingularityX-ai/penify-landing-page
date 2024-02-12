5f5284276ef7f8882c6e0edad5947b374b389d37
Suman Saurabh <ss.sumansaurabh92@gmail.com>
2024-02-13 01:24:30 +0530

    Pricing (#42)
    * pricing page added
    
    * pricing added
    
    * fix the pricing

---
+++ src/components/container/pricing/PricingOne.tsx
@@ -0,0 +1,193 @@
+import React, { forwardRef } from "react";
+import Link from "next/link";
+
+const PricingOne = forwardRef<HTMLDivElement>(function (_, ref) {
+  return (
+    <section className="section pricing-two bg-img" ref={ref}>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-12 col-xl-6">
+            <div
+              className="section__header"
+              data-aos="fade-up"
+              data-aos-duration="400"
+            >
+              <h2 className="h2">Ready to Get Started?</h2>
+            </div>
+          </div>
+        </div>
+        <div className="row items-gap">
+          <div className="col-12 col-md-6 col-lg-6 col-xxl-3">
+            <div
+              className="pricing-two__single"
+              data-aos="fade-up"
+              data-aos-duration="400"
+            >
+              <h5 className="h5">Freemium</h5>
+              <div className="meta">
+                <div className="content">
+                  <h2 className="h2 title">$0</h2>
+                  <p>paid per month</p>
+                </div>
+              </div>
+              <hr />
+              <h5 className="h5 title">Private Repo</h5>
+              <ul>
+                <li>
+                  <i className="fa-solid fa-check"></i>3 Commits Daily
+                </li>
+              </ul>
+              <hr />
+              <h5 className="h5 title">Public Repo</h5>
+              <ul>
+                <li>
+                  <i className="fa-solid fa-check"></i>Unlimited Commits
+                </li>
+              </ul>
+              <hr />
+              <ul>
+                <li>
+                  <i className="fa-solid fa-check"></i>1 Full Repo Documentation
+                </li> 
+              </ul>
+              <hr />
+              <Link href="https://dashboard.snorkell.ai/auth/sign-up" target="_blank" className="btn btn--septenary">
+                Start for free
+              </Link>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 col-lg-6 col-xxl-3">
+            <div
+              className="pricing-two__single"
+              data-aos="fade-up"
+              data-aos-duration="400"
+              data-aos-delay="200"
+            >
+              <h5 className="h5 yel">Basic</h5>
+              <div className="meta">
+                <div className="content">
+                  <h2 className="h2 title">$10</h2>
+                  <p>paid per month / repository</p>
+                </div>
+              </div>
+              <hr />
+              <h5 className="h5 title">Private Repo</h5>
+              <ul>
+                <li>
+                  <i className="fa-solid fa-check"></i>Unlimited Commits for 1 Repo
+                </li>
+                {/* <li>
+                  <i className="fa-solid fa-check"></i>1 Full Repo
+                </li> */}
+              </ul>
+              <hr />
+              <h5 className="h5 title">Public Repo</h5>
+              <ul>
+                <li>
+                  <i className="fa-solid fa-check"></i>Unlimited Commits
+                </li>
+              </ul>
+              <hr />
+              <ul>
+                <li>
+                  <i className="fa-solid fa-check"></i>3 Full Repo Documentation
+                </li> 
+              </ul>
+              <hr />
+              <Link href="https://dashboard.snorkell.ai/auth/sign-up" target="_blank" className="btn btn--septenary">
+                choose a plan
+              </Link>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 col-lg-6 col-xxl-3">
+            <div
+              className="pricing-two__single pricing-two__single--alt"
+              data-aos="fade-up"
+              data-aos-duration="400"
+              data-aos-delay="400"
+            >
+              <h5 className="h5 pri">Pro</h5>
+              <div className="meta">
+                <div className="content">
+                  <h2 className="h2 title">$45</h2>
+                  <p>paid per month / Repository</p>
+                </div>
+              </div>
+              <hr />
+              <h5 className="h5 title">Private Repo</h5>
+              <ul>
+                <li>
+                  <i className="fa-solid fa-check"></i>Unlimited Commits for 5 Repo
+                </li>
+                {/* <li>
+                  <i className="fa-solid fa-check"></i>1 Full Repo
+                </li> */}
+              </ul>
+              <hr />
+              <h5 className="h5 title">Public Repo</h5>
+              <ul>
+                <li>
+                  <i className="fa-solid fa-check"></i>Unlimited Commits
+                </li>
+              </ul>
+              <hr />
+              <ul>
+                <li>
+                  <i className="fa-solid fa-check"></i>18 Full Repo Documentation
+                </li> 
+              </ul>
+              <hr />
+              <Link href="https://dashboard.snorkell.ai/auth/sign-up" target="_blank" className="btn btn--septenary">
+                choose a plan
+              </Link>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 col-lg-6 col-xxl-3">
+            <div
+              className="pricing-two__single"
+              data-aos="fade-up"
+              data-aos-duration="400"
+              data-aos-delay="600"
+            >
+              <h5 className="h5 tri">Elite</h5>
+              <div className="meta">
+                <div className="content">
+                  <h2 className="h2 title">Contact Us For Details</h2>
+                </div>
+              </div>
+              <hr />
+              <ul>
+                <li>
+                  <i className="fa-solid fa-check"></i>On Premise Support
+                </li>
+                <li>
+                  <i className="fa-solid fa-check"></i>
+                  GPT-4 and Mistral
+                </li>
+                <li>
+                  <i className="fa-solid fa-check"></i>
+                  Private GPT-Keys
+                </li>
+                <li>
+                  <i className="fa-solid fa-check"></i>
+                  Access to Experimental Features
+                </li>
+                <li>
+                  <i className="fa-solid fa-check"></i>
+                  24*7 Support
+                </li>
+                <li></li>
+              </ul>
+              <hr />
+              <Link href="https://dashboard.snorkell.ai/auth/sign-up" target="_blank" className="btn btn--septenary">
+                choose a plan
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+});
+
+export default PricingOne;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import BannerMain from "@/components/container/banner/bannerMain";
 import FAQ from "@/components/container/faq/FAQ";
 import OverviewMid from "@/components/container/overview/overviewMid";
+import PricingOne from "@/components/container/pricing/PricingOne";
 import Tour from "@/components/container/tour/Tour";
 import Layout from "@/components/layout/Layout";
 import { useRouter } from "next/router";
@@ -9,12 +10,16 @@ import { useEffect, useRef } from "react";
 export default function Home() {
   const tourRef = useRef<HTMLDivElement>(null);
   const productRef = useRef<HTMLDivElement>(null);
+  const pricingRef = useRef<HTMLDivElement>(null);
 
   const router = useRouter();
 
   const scrollToProduct = () => {
     if (router.query.scrollTo === "exampleOverview" && productRef.current) {
       productRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+    if (router.query.scrollTo === "pricing" && pricingRef.current) {
+      pricingRef.current.scrollIntoView({ behavior: "smooth" });
     }
   };
 
@@ -32,6 +37,7 @@ export default function Home() {
       <BannerMain />
       <Tour ref={tourRef} />
       <OverviewMid ref={productRef} />
+      <PricingOne ref={pricingRef}/>
       <FAQ />
     </Layout>
   );
