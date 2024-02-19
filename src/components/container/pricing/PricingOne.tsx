30cd9858e8cbe472081b09525c621ebab0697778
sumansaurabh <ss.sumansaurabh92@gmail.com>
2024-02-19 16:29:19 +0530

    added currency information

--- src/components/container/pricing/PricingOne.tsx
+++ src/components/container/pricing/PricingOne.tsx
@@ -1,7 +1,37 @@
-import React, { forwardRef } from "react";
+import React, { useState, forwardRef, useEffect } from "react";
 import Link from "next/link";
+import { axiosInstance } from "@/config/axiosConfig";
+
+let exchangeRateUSDToINR = 82; // Example static exchange rate
 
 const PricingOne = forwardRef<HTMLDivElement>(function (_, ref) {
+  const [currency, setCurrency] = useState("INR"); // Default currency
+  const [showDropdown, setShowDropdown] = useState(false);
+  const toggleDropdown = () => setShowDropdown(!showDropdown);
+
+  const showCurrencySymbol = () => {
+    return currency === "USD" ? "$" : "₹";
+  };
+
+  const convertPrices = (priceInUSD: number) => {
+    const value = currency === "USD"
+      ? priceInUSD
+      : priceInUSD * exchangeRateUSDToINR;
+      return value.toFixed(0);
+  };
+
+  useEffect(() => {
+      axiosInstance
+        .get("/v1/analytics/conversionRates")
+        .then(({ data }) =>
+        exchangeRateUSDToINR = data
+        ).catch((error) => {
+          console.error("Error fetching conversion rates", error);
+          exchangeRateUSDToINR = 82; // Example static exchange rate
+        });
+   
+  }, []);
+
   return (
     <section className="section pricing-two bg-img" ref={ref}>
       <div className="container">
@@ -16,6 +46,54 @@ const PricingOne = forwardRef<HTMLDivElement>(function (_, ref) {
             </div>
           </div>
         </div>
+        <div className="row justify-content-center">
+          <div className="col-12 col-xl-6">
+            <div
+              className="section__header"
+              data-aos="fade-up"
+              data-aos-duration="400"
+            >
+              <div className="dropdown">
+                <button
+                  className="btn btn-secondary dropdown-toggle"
+                  type="button"
+                  onClick={toggleDropdown}
+                >
+                  Selected Currency: {currency}
+                </button>
+                {showDropdown && (
+                  <ul
+                    className="dropdown-menu show"
+                    style={{ display: "inline-block" }}
+                  >
+                    <li>
+                      <a
+                        className="dropdown-item"
+                        onClick={() => {
+                          setCurrency("USD");
+                          setShowDropdown(false);
+                        }}
+                      >
+                        USD
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="dropdown-item"
+                        onClick={() => {
+                          setCurrency("INR");
+                          setShowDropdown(false);
+                        }}
+                      >
+                        INR
+                      </a>
+                    </li>
+                  </ul>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="row items-gap">
           <div className="col-12 col-md-6 col-lg-6 col-xxl-3">
             <div
@@ -26,7 +104,7 @@ const PricingOne = forwardRef<HTMLDivElement>(function (_, ref) {
               <h5 className="h5">Freemium</h5>
               <div className="meta">
                 <div className="content">
-                  <h2 className="h2 title">$0</h2>
+                  <h2 className="h2 title">{showCurrencySymbol()}{convertPrices(0)}</h2>
                   <p>paid per month</p>
                 </div>
               </div>
@@ -48,10 +126,14 @@ const PricingOne = forwardRef<HTMLDivElement>(function (_, ref) {
               <ul>
                 <li>
                   <i className="fa-solid fa-check"></i>1 Full Repo Documentation
-                </li> 
+                </li>
               </ul>
               <hr />
-              <Link href="https://dashboard.snorkell.ai/profile/payments" target="_blank" className="btn btn--septenary">
+              <Link
+                href="https://dashboard.snorkell.ai/profile/payments"
+                target="_blank"
+                className="btn btn--septenary"
+              >
                 Start for free
               </Link>
             </div>
@@ -66,7 +148,7 @@ const PricingOne = forwardRef<HTMLDivElement>(function (_, ref) {
               <h5 className="h5 yel">Basic</h5>
               <div className="meta">
                 <div className="content">
-                  <h2 className="h2 title">$10</h2>
+                  <h2 className="h2 title">{showCurrencySymbol()}{convertPrices(10)}</h2>
                   <p>paid per month / repository</p>
                 </div>
               </div>
@@ -74,7 +156,8 @@ const PricingOne = forwardRef<HTMLDivElement>(function (_, ref) {
               <h5 className="h5 title">Private Repo</h5>
               <ul>
                 <li>
-                  <i className="fa-solid fa-check"></i>Unlimited Commits for 1 Repo
+                  <i className="fa-solid fa-check"></i>Unlimited Commits for 1
+                  Repo
                 </li>
                 {/* <li>
                   <i className="fa-solid fa-check"></i>1 Full Repo
@@ -91,10 +174,14 @@ const PricingOne = forwardRef<HTMLDivElement>(function (_, ref) {
               <ul>
                 <li>
                   <i className="fa-solid fa-check"></i>3 Full Repo Documentation
-                </li> 
+                </li>
               </ul>
               <hr />
-              <Link href="https://dashboard.snorkell.ai/profile/payments" target="_blank" className="btn btn--septenary">
+              <Link
+                href="https://dashboard.snorkell.ai/profile/payments"
+                target="_blank"
+                className="btn btn--septenary"
+              >
                 choose a plan
               </Link>
             </div>
@@ -109,7 +196,7 @@ const PricingOne = forwardRef<HTMLDivElement>(function (_, ref) {
               <h5 className="h5 pri">Pro</h5>
               <div className="meta">
                 <div className="content">
-                  <h2 className="h2 title">$45</h2>
+                  <h2 className="h2 title">{showCurrencySymbol()}{convertPrices(45)}</h2>
                   <p>paid per month / Repository</p>
                 </div>
               </div>
@@ -117,7 +204,8 @@ const PricingOne = forwardRef<HTMLDivElement>(function (_, ref) {
               <h5 className="h5 title">Private Repo</h5>
               <ul>
                 <li>
-                  <i className="fa-solid fa-check"></i>Unlimited Commits for 5 Repo
+                  <i className="fa-solid fa-check"></i>Unlimited Commits for 5
+                  Repo
                 </li>
                 {/* <li>
                   <i className="fa-solid fa-check"></i>1 Full Repo
@@ -133,11 +221,16 @@ const PricingOne = forwardRef<HTMLDivElement>(function (_, ref) {
               <hr />
               <ul>
                 <li>
-                  <i className="fa-solid fa-check"></i>18 Full Repo Documentation
-                </li> 
+                  <i className="fa-solid fa-check"></i>18 Full Repo
+                  Documentation
+                </li>
               </ul>
               <hr />
-              <Link href="https://dashboard.snorkell.ai/profile/payments" target="_blank" className="btn btn--septenary">
+              <Link
+                href="https://dashboard.snorkell.ai/profile/payments"
+                target="_blank"
+                className="btn btn--septenary"
+              >
                 choose a plan
               </Link>
             </div>
@@ -179,7 +272,11 @@ const PricingOne = forwardRef<HTMLDivElement>(function (_, ref) {
                 <li></li>
               </ul>
               <hr />
-              <Link href="https://dashboard.snorkell.ai/profile/payments" target="_blank" className="btn btn--septenary">
+              <Link
+                href="https://dashboard.snorkell.ai/profile/payments"
+                target="_blank"
+                className="btn btn--septenary"
+              >
                 choose a plan
               </Link>
             </div>
