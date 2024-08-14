f65c9c0c9998a25a64ca0c9d48a6c23a21735b9e
Mayur Dayal <mayur.dayal97@gmail.com>
2024-08-14 15:32:15 +0530

    added layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
 import { Inter } from "next/font/google";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div className={inter.className}></div>;
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleLoad = () => setIsLoading(false);
+
+    if (document.readyState === "complete") {
+      setIsLoading(false);
+    } else {
+      window.addEventListener("load", handleLoad);
+    }
+
+    return () => window.removeEventListener("load", handleLoad);
+  }, []);
+
+  return isLoading ? (
+    <div id="preloader">
+      <div className="loader"></div>
+    </div>
+  ) : (
+    <div className={inter.className}>
+      <Header />
+      <main className="my-24"></main>
+      <Footer />
+    </div>
+  );
 }
