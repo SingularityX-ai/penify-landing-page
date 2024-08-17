0386ac884f02471eafd15a29eff78f05d7423e82
Mayur Dayal <mayur.dayal97@gmail.com>
2024-08-17 22:22:02 +0530

    added meta tags. title and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,27 @@ import "@/styles/globals.css";
 import "@xyflow/react/dist/style.css";
 
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import { Fragment } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>Penify.dev | Automated Documentation Generation</title>
+        <meta
+          name="description"
+          content="Automate human-like docstring/documentation for Python, Java, TypeScript, JavaScript, Kotlin in GitHub, GitLab, Bitbucket."
+        />
+        <meta
+          name="keywords"
+          content="Penify.dev, ai docstring, Automated Documentation, GitHub Integration, Source Code Documentation, Intelligent Tracking, Smart Generation, Programming Languages, Python, JavaScript, TypeScript, Java, Kotlin, Real-Time Documentation, Privacy-Focused"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
