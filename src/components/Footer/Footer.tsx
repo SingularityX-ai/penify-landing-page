475eefcf765d8782a070e3a0b20c96d79bef9da6
Mayur Dayal <mayur.dayal97@gmail.com>
2024-08-19 10:32:52 +0530

    added animated pattern for footer background

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,10 +3,12 @@ import Link from "next/link";
 import logo from "public/penify-logo.svg";
 import { footerItems, socialIcons } from "@/utils/footerItems";
 import { Fragment } from "react";
+import patternLeft from "public/images/footer/pattern_left_bg.png";
+import patternRight from "public/images/footer/pattern_right_bg.png";
 
 export default function Footer() {
   return (
-    <footer className="w-full overflow-hidden bg-transparent py-8 md:py-16 xl:py-24">
+    <footer className="w-full overflow-hidden bg-transparent py-8 md:py-16 xl:py-24 relative">
       <div className="container mx-auto px-4">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
@@ -80,6 +82,19 @@ export default function Footer() {
           </div>
         </div>
       </div>
+
+      <Image
+        src={patternLeft}
+        loading="lazy"
+        alt="left pattern background"
+        className="animate-footer-pulse absolute left-0 top-4 md:top-12 xl:top-20"
+      />
+      <Image
+        src={patternRight}
+        loading="lazy"
+        alt="right pattern background"
+        className="animate-footer-pulse-reversed absolute bottom-16 right-0 sm:bottom-20 md:bottom-24 xl:bottom-32"
+      />
     </footer>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,24 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      animation: {
+        "footer-pulse": "pulse 6s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+        "footer-pulse-reversed":
+          "pulseReversed 6s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+      },
       colors: {
         themeBg: "#111827",
         bannerBg: "#192337",
+      },
+      keyframes: {
+        pulse: {
+          "0%, 100%": { opacity: "0.4", transform: "translateX(0)" },
+          "80%": { opacity: "1", transform: "translate(20px)" },
+        },
+        pulseReversed: {
+          "0%, 100%": { opacity: "0.4", transform: "translateX(0)" },
+          "80%": { opacity: "1", transform: "translate(-20px)" },
+        },
       },
     },
   },
