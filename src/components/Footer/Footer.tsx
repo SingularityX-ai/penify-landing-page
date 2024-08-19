dc80762ef21345d77c66f3124b41175eb808a243
Mayur Dayal <mayur.dayal97@gmail.com>
2024-08-19 09:46:30 +0530

    added responsive font size for footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,39 +6,46 @@ import { Fragment } from "react";
 
 export default function Footer() {
   return (
-    <footer className="bg-white">
-      <div className="container mx-auto p-4 py-6 lg:py-8">
+    <footer className="w-full overflow-hidden bg-transparent py-8 md:py-16 xl:py-24">
+      <div className="container mx-auto px-4">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
             <Link href="/" className="inline-block">
-              <Image src={logo} className="w-full h-10" alt="Penify Logo" />
+              <Image
+                src={logo}
+                className="xl:h-13 h-9 w-full md:h-11"
+                alt="Penify Logo"
+              />
             </Link>
           </div>
 
-          <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 sm:gap-6 md:grid-cols-3 w-full md:w-4/5">
+          <div className="grid w-full grid-cols-1 gap-8 sm:grid-cols-2 sm:gap-6 md:w-4/5 md:grid-cols-3">
             {footerItems.map(({ title, children }, footerIndex) => (
               <div key={`footer-menu-${footerIndex}`}>
-                <h2 className="mb-6 text-sm font-semibold uppercase text-gray-900">
+                <h2 className="mb-6 text-sm font-semibold uppercase text-slate-200 md:text-base xl:text-lg">
                   {title}
                 </h2>
 
-                <ul className="font-medium text-gray-500">
+                <ul className="font-medium text-slate-300">
                   {children.map(({ title, href, mode }, childIndex) => (
                     <li
                       key={`footer-sub-menu-${childIndex}`}
-                      className="mb-4 last:mb-0"
+                      className="mb-4 last:mb-0 text-xs md:text-sm xl:text-base"
                     >
                       {mode === "external" ? (
                         <Link
                           href={href}
                           target="_blank"
                           rel="noopener noreferrer"
-                          className="hover:underline"
+                          className="inline-block transition-all duration-200 ease-in hover:translate-x-2 hover:text-blue-700 hover:underline"
                         >
                           {title}
                         </Link>
                       ) : (
-                        <Link href={href} className="hover:underline">
+                        <Link
+                          href={href}
+                          className="inline-block transition-all duration-200 ease-in hover:translate-x-2 hover:text-blue-700 hover:underline"
+                        >
                           {title}
                         </Link>
                       )}
@@ -50,19 +57,19 @@ export default function Footer() {
           </div>
         </div>
 
-        <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+        <hr className="my-6 border-slate-600 sm:mx-auto lg:my-8" />
 
-        <div className="sm:flex sm:items-center sm:justify-between sm:flex-wrap">
-          <p className="text-sm text-gray-500 sm:text-center">
+        <div className="sm:flex sm:flex-wrap sm:items-center sm:justify-center md:justify-between">
+          <p className="text-sm text-slate-400 sm:text-center md:text-base xl:text-lg">
             © 2023 Snorkell Associates and Co. All Rights Reserved.
           </p>
 
-          <div className="mt-4 flex sm:mt-0 sm:justify-center">
+          <div className="mt-4 flex sm:justify-center md:mt-0">
             {socialIcons.map(({ title, icon, href }, socialIconIndex) => (
               <Fragment key={`social-icon-${socialIconIndex}`}>
                 <Link
                   href={href}
-                  className="text-gray-500 hover:text-gray-900 mx-3"
+                  className="mx-3 text-slate-400 hover:text-slate-500"
                 >
                   {icon}
 
